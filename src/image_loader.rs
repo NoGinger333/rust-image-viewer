c3f7b9320b9f93bf6be4cc6cb21f394efee900fc
User <user@example.com>
2026-08-11 18:54:14 +0900

    Always apply unsharpen mask for crisp text in high-res manga images

--- src/image_loader.rs
+++ src/image_loader.rs
@@ -65,11 +65,17 @@ impl LoadedImage {
             img = img.flipv();
         }
 
-        let (w, h) = img.dimensions();
-        let color_img = dynamic_image_to_color_image(&img);
+        // マンガのセリフ・細線の輪郭をクッキリ保つ常時シャープネス補正 (アンシャープマスク)
+        let sharpened_buf = image::imageops::unsharpen(&img, 1.2, 1);
+        let sharpened = DynamicImage::ImageRgba8(sharpened_buf);
+
+        let (w, h) = sharpened.dimensions();
+        let color_img = dynamic_image_to_color_image(&sharpened);
         (color_img, w, h)
     }
 }
+
+
 
 /// DynamicImage から egui::ColorImage への高精度変換
 fn dynamic_image_to_color_image(img: &DynamicImage) -> ColorImage {
